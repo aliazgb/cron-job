5ba85a1583d9cd92fffe4c6b98ab695d6e88037e
aliazgb <ali.azgb10@gmail.com>
2025-09-09 04:23:39 +0200

    Initial commit

---
+++ cron-job.js
@@ -0,0 +1,53 @@
+import fetch from "node-fetch";
+
+const usersblog = [
+  { email: "user@test.com", password: "12345678" },
+  { email: "user@test1.com", password: "12345678" },
+  { email: "user@test2.com", password: "12345678" },
+];
+
+const userFreelancerApp = [
+  { phoneNumber: "015222222222" },
+  { phoneNumber: "015233333333" },
+  { phoneNumber: "015211111111" },
+];
+
+function getRandomItem(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+async function run() {
+  try {
+    const randomUser = getRandomItem(usersblog);
+
+    const signinRes = await fetch(
+      "https://api.blog-app.online/api/user/signin",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(randomUser),
+      }
+    );
+    const signinData = await signinRes.json();
+
+    const randomPhone = getRandomItem(userFreelancerApp);
+
+    const otpRes = await fetch(
+      "https://freelanceringapp-backend-feqf.onrender.com/api/user/get-otp",
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(randomPhone),
+      }
+    );
+
+    const otpData = await otpRes.json();
+
+    console.log("Signin:", signinData);
+    console.log("OTP:", otpData);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+}
+
+run();
